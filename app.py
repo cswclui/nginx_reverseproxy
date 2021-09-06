aa9507c2a85a86b5ba60b6eb77d06a4a53dac166
cswclui <cswclui@polyu.edu.hk>
2021-09-06 16:56:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, My container is named: " + socket.gethostname()
+    return "Hello, My container is named: " + socket.gethostname() + "\n"
